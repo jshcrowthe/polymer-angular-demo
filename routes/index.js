89a0d12e86cefae5157566b3eb2925cbffba670a
Josh Crowther <jshcrowthe@gmail.com>
2015-05-25 20:16:26 -0600

    Update demo

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/:view', function(req, res) {
+  res.render(req.params.view, { title: 'Demo' });
+});
+
 module.exports = router;
